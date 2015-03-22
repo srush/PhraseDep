74b0232d662fef2899e381b32e29c97be99beb76
ikekonglp <ikekonglp@gmail.com>
2015-03-21 21:25:34 -0400

    one more option

--- PhraseStructureTreeConverter/run_command.sh
+++ PhraseStructureTreeConverter/run_command.sh
@@ -11,4 +11,4 @@ runtime_classpath=${SCRIPTPATH}/bin:${Stanford_Parser_JAR}
 
 javac -classpath ${Stanford_Parser_JAR} -sourcepath src -d bin src/StanfordHeadRuleFinder.java 
 
-java -Dfile.encoding=UTF-8 -classpath ${runtime_classpath} StanfordHeadRuleFinder $1 $2 $3 $4 $5 $6
+java -Dfile.encoding=UTF-8 -classpath ${runtime_classpath} StanfordHeadRuleFinder $1 $2 $3 $4 $5 $6 $7
